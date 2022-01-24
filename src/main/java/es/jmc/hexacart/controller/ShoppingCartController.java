f39e6dd2db3594e63f5d0471f4eb84469a42d6e0
juanmi <jmcabrera@copado.com>
2022-01-24 19:31:06 +0100

    Controller pending calls

--- src/main/java/es/jmc/hexacart/controller/ShoppingCartController.java
+++ src/main/java/es/jmc/hexacart/controller/ShoppingCartController.java
@@ -1,12 +1,15 @@
 package es.jmc.hexacart.controller;
 
+import static java.lang.String.format;
+
 import es.jmc.hexacart.service.ShoppingCartService;
-import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ShoppingCartController {
 
+  private static final String ID = "{id}";
+  private static final String PROD_ID = "prod_id";
+  private static final String NUM_OF_PROD = "prod_quantity";
+  private static final String PRODUCT_CART_PATH = ID + "/product/{" + PROD_ID + "}";
+  private static final String NUM_OF_PRODUCT_CART_PATH = ID + "/product/{" + PROD_ID + "}/quantity/{" + NUM_OF_PROD + "}";
+
   private final ShoppingCartService service;
 
   @PostMapping()
@@ -25,7 +34,7 @@ public class ShoppingCartController {
     return ResponseEntity.ok(service.createShoppingCart());
   }
 
-  @GetMapping("{id}")
+  @GetMapping(ID)
   public ResponseEntity<ShoppingCartFullResponse> getShoppingCart(@PathVariable long id) {
 
     ShoppingCartFullResponse response = service.getShoppingCart(id);
@@ -33,12 +42,42 @@ public class ShoppingCartController {
     return ResponseEntity.ok(response);
   }
 
-  @DeleteMapping("{id}")
+  @DeleteMapping(ID)
   public ResponseEntity deleteShoppingCart(@PathVariable long id) {
 
     service.deleteShoppingCart(id);
 
     return ResponseEntity.ok().build();
   }
+
+  @PatchMapping(ID)
+  public ResponseEntity completeShoppingCart(@PathVariable long id) {
+
+    // TODO
+
+    return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+  }
+
+  @PostMapping(NUM_OF_PRODUCT_CART_PATH)
+  public ResponseEntity addProductToShoppingCart(
+      @PathVariable long id,
+      @PathVariable(PROD_ID) long productId,
+      @PathVariable(NUM_OF_PROD) int quantity) {
+
+    // TODO
+
+    return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+  }
+
+  @DeleteMapping(PRODUCT_CART_PATH)
+  public ResponseEntity removeProductFromShoppingCart(
+      @PathVariable long id,
+      @PathVariable(PROD_ID) long productId) {
+
+    // TODO
+
+    return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).build();
+  }
+
 
 }
